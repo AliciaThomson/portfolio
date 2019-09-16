223e731af42a696e57d03b638a6fa8a0013c4e34
Alicia Thomson <alicia@madhattertech.ca>
2019-09-16 10:04:33 -0400

    Updates

---
+++ precache-manifest.91c8725e3942f269abe304c34bd4a791.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ed6c8491411dfe5ad3a1c7b873ef1a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "12a7632e6ce6e92fac64",
+    "url": "/portfolio/static/css/main.571aa76f.chunk.css"
+  },
+  {
+    "revision": "9c2650060ff1fbca522d",
+    "url": "/portfolio/static/js/2.0c4502fd.chunk.js"
+  },
+  {
+    "revision": "12a7632e6ce6e92fac64",
+    "url": "/portfolio/static/js/main.1549c643.chunk.js"
+  },
+  {
+    "revision": "7d277289d5425eff09b8",
+    "url": "/portfolio/static/js/runtime~main.f0586756.js"
+  },
+  {
+    "revision": "e8a791e512ee9324b35e8498b5fd09c0",
+    "url": "/portfolio/static/media/bubble-1.e8a791e5.svg"
+  },
+  {
+    "revision": "e11c46e927e1bf60097c469f91f14fbb",
+    "url": "/portfolio/static/media/bubble-2.e11c46e9.svg"
+  },
+  {
+    "revision": "c165ef410124ce9eebe491ceefdaf589",
+    "url": "/portfolio/static/media/huron-bg.c165ef41.jpg"
+  },
+  {
+    "revision": "0ab2582ceecc676e9cf3ad0330873e5f",
+    "url": "/portfolio/static/media/huron-u-logo.0ab2582c.svg"
+  },
+  {
+    "revision": "76787c1f6be541cded936f82b96b950d",
+    "url": "/portfolio/static/media/icon-accountable.76787c1f.svg"
+  },
+  {
+    "revision": "e849563e98e573a63314a6e81949ef53",
+    "url": "/portfolio/static/media/icon-collaboration.e849563e.svg"
+  },
+  {
+    "revision": "de8a93a3fcf89b0146c5c35c17dfcd9d",
+    "url": "/portfolio/static/media/icon-communication.de8a93a3.svg"
+  },
+  {
+    "revision": "dd3997afa1d7dd9f22eb03f27407b8ae",
+    "url": "/portfolio/static/media/icon-magnifying-glass.dd3997af.svg"
+  },
+  {
+    "revision": "2217b919e168650d0bae97873b143abf",
+    "url": "/portfolio/static/media/leaves-illustration.2217b919.svg"
+  },
+  {
+    "revision": "17f6ef9e447080ad22b46f3985585c7f",
+    "url": "/portfolio/static/media/project-element-1.17f6ef9e.svg"
+  },
+  {
+    "revision": "a47df5b984ed71b59ebdda6985a6fb40",
+    "url": "/portfolio/static/media/project-element-2.a47df5b9.svg"
+  },
+  {
+    "revision": "634b4990d7917647344f41dd1cb8f9ce",
+    "url": "/portfolio/static/media/self-portrait.634b4990.svg"
+  },
+  {
+    "revision": "28208f903ea4e7856d26079a6d372509",
+    "url": "/portfolio/static/media/skills-bg.28208f90.svg"
+  },
+  {
+    "revision": "7c475901e122c3c742dc1cc40590c44d",
+    "url": "/portfolio/static/media/tps-bg.7c475901.jpg"
+  },
+  {
+    "revision": "07b06b49aa6e810f6cee33dd3b975be0",
+    "url": "/portfolio/static/media/tps-logo.07b06b49.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92ddd8606a141339edfc60cc8338a81f.js"
+  "/portfolio/precache-manifest.91c8725e3942f269abe304c34bd4a791.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
